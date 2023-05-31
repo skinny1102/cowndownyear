47129cefd80827bb88f69fe7f904b6525f15441b
THANG Nguyen Huu (VTI.D1) <thang.nguyenhuu@vti.com.vn>
2023-05-31 21:12:08 +0700

    fix

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-var day = document.getElementById("days")
-var hour = document.getElementById("hours")
-var min = document.getElementById("mins")
-var second = document.getElementById("seconds")
-
-const newYears = '1 Jan 2022'
-function countdown(){
-    const newYearsDate= new Date(newYears)
-    const currentDate = new Date()
-    const totalseconds = (newYearsDate-currentDate)/1000;
-    const days = Math.floor(totalseconds/3600/24)
-    const hours = Math.floor(totalseconds/3600)%24
-    const minutes = (Math.floor(totalseconds/60)%60)
-    const seconds =Math.floor(totalseconds%60)
-    console.log(hours)
-    day.innerHTML=days
-    hour.innerHTML=hours
-    mins.innerHTML=minutes
-    second.innerHTML=seconds
+var day = document.getElementById("days");
+var hour = document.getElementById("hours");
+var min = document.getElementById("mins");
+var second = document.getElementById("seconds");
+var year = document.getElementById("year");
+function countdown() {
+  const yearNew = new Date().getFullYear();
+  let newYears = "1 Jan " + Number(yearNew + 1);
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+  const totalseconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalseconds / 3600 / 24);
+  const hours = Math.floor(totalseconds / 3600) % 24;
+  const minutes = Math.floor(totalseconds / 60) % 60;
+  const seconds = Math.floor(totalseconds % 60);
+  day.innerHTML = days;
+  hour.innerHTML = hours;
+  mins.innerHTML = minutes;
+  second.innerHTML = seconds;
+  year.innerHTML = 'Countdown NewYear '+(yearNew+1)
 }
-setInterval(countdown,1000)
+setInterval(countdown, 1000);
